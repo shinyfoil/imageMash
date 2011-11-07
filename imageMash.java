17ab57d6d62dbc15b1e4ad96d4f789d35d79e0b6
Richard Mah <dick.mah@gmail.com>
2011-11-06 23:20:27 -0500

    fixed some things, moving back to test

--- imageMash.java
+++ imageMash.java
@@ -5,15 +5,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.StringTokenizer;
 
 import javax.imageio.ImageIO;
 
 public class imageMash {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
 		// Check to see if there's actually a list file.
@@ -25,55 +23,72 @@ public class imageMash {
 		// Where we're storing the desired combinations
 		List<String[]> combos;
 		
-		// Try loading the information into the combos
 		try {
-			BufferedReader a = new BufferedReader(new FileReader(args[0]));
-			combos = getFiles(a);
-			a.close();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
 		}
 		
-		
-		
-		BufferedImage out = null;
-		
-		BufferedImage face = null;
-		try {
-			face = ImageIO.read(new File("file.jpg"));
-		}
-		catch(IOException e) {
-			// EXCEPTION HANDLING HERE
-		}
-		
-		BufferedImage house = null;
-		try {
-			house = ImageIO.read(new File("file.jpg"));
-		}
-		catch(IOException e) {
-			// EXCEPTION HANDLING HERE
+		// Try loading the combos file
+		if(reader == null)
+			System.out.println("No tokens were found. Check your file.");
+		else {
+				while(reader.ready()) {
+					String currentLine = reader.readLine();
+					
+					StringTokenizer b = new StringTokenizer(currentLine, "\t");
+					String[] n = new String[3];
+					
+					for(int i = 0; i < 3; i++)
+						n[i] = b.nextToken();
+					combos.add(n);
+				}
 		}
 		
-		out = doMash(face, house);
+		File in = new File(args[0]);
+		String path = in.getPath();
 		
-		try {
-			ImageIO.write(out, "JPEG", new File("out.jpg"));
-		}
-		catch(IOException e) {
-			// EXCEPTION HANDLING
-		}
-		catch(IllegalArgumentException e) {
-			// EXCEPTION HANDING
-		}
+		// Get ready to crunch through all of the combinations
+		ListIterator<String[]> place = combos.listIterator();
 		
+		while(place.hasNext()) {
+			String[] current = place.next();
+			
+			BufferedImage out = null;
+			
+			BufferedImage face = null;
+			try {
+				face = ImageIO.read(new File(path + current[1] + ".jpg"));
+			}
+			catch(IOException e) {
+				// EXCEPTION HANDLING
+				e.printStackTrace()
+			}
+			
+			BufferedImage house = null;
+			try {
+				house = ImageIO.read(new File(path + current[2] + ".jpg"));
+			}
+			catch(IOException e) {
+				// EXCEPTION HANDLING
+				e.printStackTrace()
+			}
+			
+			out = doMash(face, house);
+			
+			try {
+				ImageIO.write(out, "JPEG", new File(path + current[0] + ".jpg"));
+			}
+			catch(IOException e) {
+				// EXCEPTION HANDLING
+				e.printStackTrace()
+			}
+			catch(IllegalArgumentException e) {
+				// EXCEPTION HANDING
+				e.printStackTrace()
+			}
+		}
 	}
 	
 	public static BufferedImage doMash(BufferedImage face, BufferedImage house) {
-		
 		// make a buffimg for output
 		BufferedImage output = new BufferedImage(200, 200, BufferedImage.TYPE_INT_RGB);
 		// something to give us the flipflip effect for the loop
@@ -94,27 +109,6 @@ public class imageMash {
 			}
 			i++;
 		}
-		
 		return output;
-	}
-	
-	public static List<String[]> getFiles(BufferedReader a) throws IOException {
-		
-		List<String[]> o = null;
-		
-		if(a == null)
-			System.out.println("No tokens were found. Check your file.");
-		else {
-				while(a.ready()) {
-					String currentLine = a.readLine();
-					
-					StringTokenizer b = new StringTokenizer(currentLine, "\t");
-					String[] n = new String[3];
-					
-					for(int i = 0; i < 3; i++)
-						n[i] = b.nextToken();
-				}
-		}
-		return o;
 	}
 }
